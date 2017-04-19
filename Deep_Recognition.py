9400fcd4be990f0e82c69365ad2dcdfe009d1100
kaushik shingne <kaushikshingne18@gmail.com>
2017-04-19 16:29:04 -0700

    added initial code for uploads

--- Deep_Recognition.py
+++ Deep_Recognition.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from celery import Celery
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/register_user')
+def user_registration():
+    return render_template('register_user.html')
+
+
+@app.route('/validate_user')
+def validate_user():
+    return ''
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'file uploaded successfully'
 
 
 if __name__ == '__main__':
