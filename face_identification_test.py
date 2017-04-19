cd3f1b7a1cd7ac8019be530daa42ebd4fc39799c
kaushik shingne <kaushikshingne18@gmail.com>
2017-04-19 14:49:00 -0700

    added initial files

---
+++ face_identification_test.py
@@ -0,0 +1,19 @@
+import face_recognition
+
+client_image = face_recognition.load_image_file("/home/kaushik/Pictures/b.jpg")
+test_image = face_recognition.load_image_file("/home/kaushik/Pictures/g1.jpg")
+client_face_loc = face_recognition.face_locations(client_image)
+test_face_loc = face_recognition.face_locations(test_image)
+
+print("found {} face(s) in client photograph.".format(len(client_face_loc)))
+print("found {} face(s) in test photograph.".format(len(test_face_loc)))
+
+
+# assuming that there is only one face in each image
+
+client_image_encoding = face_recognition.face_encodings(client_image)[0]
+test_image_encoding = face_recognition.face_encodings(test_image)[0]
+
+result = face_recognition.compare_faces([client_image_encoding],test_image_encoding)
+
+print("Does this face match ? ",result)
---
+++ new_user_encoder.py
@@ -0,0 +1,16 @@
+# create face encodings of uploaded images when we register a new user and save the results
+# we could take up to 3 images
+
+
+import face_recognition
+
+
+class User_Uploader :
+
+    def __init__(self , source_folder):
+        self.source_folder  = source_folder
+
+
+
+
+
