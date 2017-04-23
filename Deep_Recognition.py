44c731490298372ca66837146e895742adc318b2
kaushik shingne <kaushikshingne18@gmail.com>
2017-04-22 20:03:29 -0700

    initial commit for checking faces

--- Deep_Recognition.py
+++ Deep_Recognition.py
@@ -1,8 +1,34 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import os
 from celery import Celery
+import face_checker
 
 app = Flask(__name__)
+# TODO config locations should be from seperate config files
+app.config['UPLOAD_FOLDER'] = "/home/kaushik/Downloads/"
+app.config['ALLOWED_EXTENSIONS'] = ["png", 'jpg', 'jpeg']
+
+
+@app.route('/validate_user')
+def validate_user_form():
+    return render_template('validate_user.html')
+
+
+@app.route('/validator', methods=['POST'])
+def validate_user():
+    if 'file' not in request.files:
+        return 'No File Selected !'
+
+    file = request.files['file']
+    user_name = request.form['user_name']
+    if file and allowed_file(file.filename):
+        checks = face_checker.Face_Checker()
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], user_name)
+        if os.path.exists(file_path):
+         return str(checks.decode_compare_face(file_to_validate=file, user_path= file_path))
+        else :
+            return "User does not exist !"
 
 
 @app.route('/register_user')
@@ -10,17 +36,28 @@ def user_registration():
     return render_template('register_user.html')
 
 
-@app.route('/validate_user')
-def validate_user():
-    return ''
-
-
-@app.route('/uploader', methods=['GET', 'POST'])
+# TODO send JSON responses ; process files to check for face present
+# TODO strategy for uploading multiple image files during registration
+# handles user registration and file upload
+@app.route('/uploader', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+    if 'file' not in request.files:
+        return 'No File Selected !'
+
+    file = request.files['file']
+    uploader_name = request.form['user_name']
+    if file and allowed_file(file.filename):
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploader_name)
+        os.makedirs(file_path, exist_ok=True)
+        file.save(os.path.join(file_path,"1.jpg"))
+        return 'File uploaded sucessfully !'
+    else:
+        return "Invalid File !"
+
+
+def allowed_file(file_name):
+    return "." in file_name and \
+           file_name.rsplit(".", 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
 if __name__ == '__main__':
--- face_checker.py
+++ face_checker.py
@@ -1,17 +1,29 @@
 # create face encodings of uploaded images when we register a new user and save the results
 # we could take up to 3 images
 
-
 import face_recognition
 import zipfile
-
-class User_Uploader :
-
-    def __init__(self , source_folder):
-        self.source_folder  = source_folder
+import Deep_Recognition
+import os
 
 
+class Face_Checker:
+    # TODO this is only a testing method ; need to add bettter and more methods
 
+    def decode_compare_face(self, file_to_validate, user_path):
+        client_image = face_recognition.load_image_file(os.path.join(user_path, "1.jpg"))
+        test_image = face_recognition.load_image_file(file_to_validate)
+        client_face_loc = face_recognition.face_locations(client_image)
+        test_face_loc = face_recognition.face_locations(test_image)
 
+        if (len(client_face_loc) != 1):
+            return "Invalid   image  in records !"
 
+        if (len(test_face_loc) != 1):
+            return "Invalid image for testing !"
 
+        # assuming that there is only one face in each image
+        client_image_encoding = face_recognition.face_encodings(client_image)[0]
+        test_image_encoding = face_recognition.face_encodings(test_image)[0]
+        result = face_recognition.compare_faces([client_image_encoding], test_image_encoding)
+        return result[0]
